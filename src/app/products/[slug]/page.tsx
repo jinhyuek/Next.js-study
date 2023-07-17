f2ca66702693e162dfd62e62545ecfa699f7298b
jinhyuk <jjh943202@naver.com>
2023-07-17 18:01:11 +0900

    second pj

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+export default function PantsPage({ params }: Props) {
+  return <h1>{params.slug}바지 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  //미리 생성 해둠. 빌드시 원하는 동적페이지를 미리 만들어 놓을 수 있도록 설정.
+  const products = ["skirt", "pants"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
